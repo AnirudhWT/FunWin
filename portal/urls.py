159b726bdb9a0d43f6e62026f833d2162d8f120b
AnirudhWT <anirudh.khatwani@walkingtree.tech>
2020-09-24 00:18:06 +0530

    digital-solution page started

--- portal/urls.py
+++ portal/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from portal import views
 
 urlpatterns = [
-     path("",views.index,),
+     path("",views.index,name='home'),
+     path('digitalsolution',views.digitalsolution,name="digitalsolution")
 ]
